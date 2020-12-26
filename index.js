a8fb05a66913e68e61f9157f0c2e0576ab7f5c11
kevinMoreland <kmmorela@calpoly.edu>
2020-12-26 14:37:27 -0800

    Added reprompt to keep skill session open

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const LaunchHandler = {
       .speak(speakOutput)
       // Uncomment the next line if you want to keep the session open so you can
       // ask for another fact without first re-opening the skill
-      // .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
       .getResponse();
   },
 };
