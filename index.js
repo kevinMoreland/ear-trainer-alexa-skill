f6ebaf72dce927d686c6b7eaf8ad790a207cc346
kevinMoreland <kmmorela@calpoly.edu>
2021-01-05 20:01:57 -0800

    Added helper function to find slot value

--- index.js
+++ index.js
@@ -172,6 +172,48 @@ const ErrorHandler = {
   },
 };
 
+function getSlotValues(filledSlots) {
+    const slotValues = {};
+
+    Object.keys(filledSlots).forEach((item) => {
+        const name  = filledSlots[item].name;
+
+        if (filledSlots[item] &&
+            filledSlots[item].resolutions &&
+            filledSlots[item].resolutions.resolutionsPerAuthority[0] &&
+            filledSlots[item].resolutions.resolutionsPerAuthority[0].status &&
+            filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
+            switch (filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
+                case 'ER_SUCCESS_MATCH':
+                    slotValues[name] = {
+                        heardAs: filledSlots[item].value,
+                        resolved: filledSlots[item].resolutions.resolutionsPerAuthority[0].values[0].value.name,
+                        ERstatus: 'ER_SUCCESS_MATCH'
+                    };
+                    break;
+                case 'ER_SUCCESS_NO_MATCH':
+                    slotValues[name] = {
+                        heardAs: filledSlots[item].value,
+                        resolved: '',
+                        ERstatus: 'ER_SUCCESS_NO_MATCH'
+                    };
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            slotValues[name] = {
+                heardAs: filledSlots[item].value,
+                resolved: '',
+                ERstatus: ''
+            };
+        }
+    }, this);
+
+    return slotValues;
+} 
+
+
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
